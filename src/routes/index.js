be66b6b7482909efd0153e0044b0a8eb90eb2df8
Luz Clara Mora Salazar <luzclara3011@estudiantec.cr>
2024-04-12 11:17:41 -0600

    finalización del BACKEND

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,10 @@
 const {Router} = require('express');
 const router = Router();
-const {getAllTask,createTask, updateTask, deleteTask, getTaskById} = require('../controllers/tareas.controllers');
+const {getAllTask,createTask, updateTask, deleteTask} = require('../controllers/tareas.controllers');
 const {getUsers} = require('../controllers/colaboradores.controllers');
 
 router.get('/users', getUsers);
 router.get('/task', getAllTask);
-router.get('/task/:id', getTaskById);
 router.post('/task', createTask);
 router.put('/task/:id', updateTask);
 router.delete('/task/:id', deleteTask);
